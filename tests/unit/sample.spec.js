4cfea0ac8cefa033c6d454b26763088130377c46
Fulano2141 <macuto2142dt@gmail.com>
2020-09-05 17:34:27 -0400

    s

--- tests/unit/sample.spec.js
+++ tests/unit/sample.spec.js
@@ -19,7 +19,7 @@ test('but there is a "stop" in Christoph', () => {
 });
 
 test('adding floating point numbers', () => {
-    const value = 0.1 + 0.6;
+    const value = 0.1 + 0.2;
     // expect(value).toBe(0.3); //          This won't work because of rounding error
     expect(value).toBeCloseTo(0.3); // This works.
   });
