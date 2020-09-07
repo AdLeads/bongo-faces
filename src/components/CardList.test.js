ce4d7cb4010bd4e726aac3459721731a25b007e7
Fulano2141 <macuto2142dt@gmail.com>
2020-09-06 21:53:38 -0400

    coveralls

--- src/components/CardList.test.js
+++ src/components/CardList.test.js
@@ -1,38 +1,38 @@
-import axios from 'axios';
-import CardList from './CardList.vue'
-jest.mock('axios');
+// import axios from 'axios';
+// import CardList from './CardList.vue'
+// jest.mock('axios');
 
-describe('CardList.vue', () => {
-    test('should fetch users', () => {
-        const users = [{
-            title: "Pre-fab homes",
-            description: "Fulano",
-            key: 1,
-            flex: 12
-        }];
-        const resp = { data: users };
-        axios.get.mockResolvedValue(resp);
+// describe('CardList.vue', () => {
+//     test('should fetch users', () => {
+//         const users = [{
+//             title: "Pre-fab homes",
+//             description: "Fulano",
+//             key: 1,
+//             flex: 12
+//         }];
+//         const resp = { data: users };
+//         axios.get.mockResolvedValue(resp);
 
-        // or you could use the following depending on your use case:
-        // axios.get.mockImplementation(() => Promise.resolve(resp))
-        const data =CardList.methods.fetchItems();
-        return data => expect(data).toEqual(users);
-    });
-})
+//         // or you could use the following depending on your use case:
+//         // axios.get.mockImplementation(() => Promise.resolve(resp))
+//         const data =CardList.methods.fetchItems();
+//         return data => expect(data).toEqual(users);
+//     });
+// })
 
 
 
-// ========================= GENERIC TEST =====================================
-// test('there is no I in team', () => {
-//     expect('team').not.toMatch(/I/);
-// });
+// // ========================= GENERIC TEST =====================================
+// // test('there is no I in team', () => {
+// //     expect('team').not.toMatch(/I/);
+// // });
 
-// test('but there is a "stop" in Christoph', () => {
-//     expect('Christoph').toMatch(/stop/);
-// });
+// // test('but there is a "stop" in Christoph', () => {
+// //     expect('Christoph').toMatch(/stop/);
+// // });
 
-// test('adding floating point numbers', () => {
-//     const value = 0.1 + 0.2;
-//     // expect(value).toBe(0.3); //          This won't work because of rounding error
-//     expect(value).toBeCloseTo(0.3); // This works.
-// });
+test('adding floating point numbers', () => {
+    const value = 0.1 + 0.2;
+    // expect(value).toBe(0.3); //          This won't work because of rounding error
+    expect(value).toBeCloseTo(0.3); // This works.
+});
