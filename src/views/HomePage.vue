d6d67dc082a1d5ac7728a44bc109c2f280e12e6b
Fulano2141 <macuto2142dt@gmail.com>
2021-01-23 17:34:57 -0400

    last version

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -11,36 +11,35 @@
 </template>
 
 <script>
-import json from "../external/files.json";
-// import axios from "axios";
+// import json from "../external/files.json";
+import axios from "axios";
 import Card from "../components/Card.vue";
 export default {
   data: () => ({
-    cards: json,
-    // cards: [],
+    // cards: json,
+    cards: [],
     url: "https://bongo-engine-staging.herokuapp.com/posts",
   }),
   components: {
     Card,
   },
-  // mounted() {
-  //   this.fetchItems();
-  // },
-  // methods: {
-  //   fetchItems() {
-  //     axios
-  //       .get(this.url)
-  //       .then((response) => {
-  //         this.cards = response.data;
-  //         console.log(this.cards);
-  //         this.expand = true;
-  //       })
-  //       .catch((error) => {
-  //         console.log(error);
-  //       });
-  //   },
-  // },
-  
+  mounted() {
+    this.fetchItems();
+  },
+  methods: {
+    fetchItems() {
+      axios
+        .get(this.url)
+        .then((response) => {
+          this.cards = response.data;
+          console.log(this.cards);
+          this.expand = true;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
 };
 </script>
 <style scoped>
